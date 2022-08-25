85755f9eff01491762d2d772401a61be16d68366
timcj030 <52206302+timcj030@users.noreply.github.com>
2022-08-25 15:24:54 +1000

    Update string_forloop.js
    modified the function

--- string_forloop.js
+++ string_forloop.js
@@ -8,12 +8,28 @@ print('The length of the entered value is {0}'.format(len(strval)))
 for i in strval:
     print(i)
 
+print('The string in revere order is ')
+
+#function to reverse the string
+def reverse(text):
+    for l in range(1, len(text) + 1):
+        text[len(text) - l]
+        print(text[len(text) - l])
+
+#call the functions to reverse the text
+reverse(strval)
+
+
+
+
+
+
 #reverse the string
-revstr=""
-for j in strval:
-    revstr = j + revstr
-print('The string in revere is {0}'.format(revstr))
+#revstr=""
+#for j in strval:
+    #revstr = j + revstr
+#print('The string in revere is {0}'.format(revstr))
 
 # print the characters in revere order
-for k in revstr:
-    print(k)
+#for k in revstr:
+   # print(k)
